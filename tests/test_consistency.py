cfe7b039721a5f4fbbd4db6be1180d9d9d8860b1
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-03-28 03:19:20 +0100

    Refactor code for speed and clarity

--- tests/test_consistency.py
+++ tests/test_consistency.py
@@ -1,11 +1,10 @@
 # Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
 
+import clip
 import numpy as np
 import pytest
 import torch
 from PIL import Image
-
-import clip
 
 
 @pytest.mark.parametrize("model_name", clip.available_models())
